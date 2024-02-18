aa345a277cde585d47fc7c447e902d5e5f4474c5
azitta <kjtakio30147@gmail.com>
2024-02-18 21:33:20 +0900

    お試し

---
+++ client/Services/Appointment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TryBlazor
+{
+    public class Appointment
+    {
+        public long ID { get; set; }
+        public string Text { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+    }
+}
